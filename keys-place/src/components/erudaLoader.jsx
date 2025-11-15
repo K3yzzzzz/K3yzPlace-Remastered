1fd5a5a9c7b944743a8c31b0575af42ca0707e38
K3yzzzzz <Maddoxrileyaz@gmail.com>
2025-11-15 10:21:16 +0000

    0.0.1.1

--- keys-place/src/components/erudaLoader.jsx
+++ keys-place/src/components/erudaLoader.jsx
@@ -2,15 +2,19 @@ import { useEffect } from "react";
 
 const ErudaLoader = () => {
   useEffect(() => {
-    if (process.env.NODE_ENV === "development") {
-      const script = document.createElement("script");
-      script.src = "https://cdn.jsdelivr.net/npm/eruda";
-      script.onload = () => {
-        window.eruda.init();
-      };
-      document.body.appendChild(script);
-    }
+    if (process.env.NODE_ENV !== "development") return;
+
+    if (window.__ERUDA_LOADED__) return;
+    window.__ERUDA_LOADED__ = true;
+
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/eruda";
+    script.onload = () => {
+      window.eruda.init();
+    };
+    document.body.appendChild(script);
   }, []);
+
 
   return null;
 };
